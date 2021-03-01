5f75373cdb0ea30baa04b6705b501862a833d7d9
vodinhnguyen <nguyenvabu@gmail.com>
2021-03-01 17:35:42 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Train.py

--- Train.py
+++ Train.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-import itertools
+import cv2
+import pickle
 from glob import glob
 from PIL import Image
-from sklearn.metrics import confusion_matrix
-from  sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
@@ -24,20 +24,20 @@ crack_Negative_df['path'] = imageid_path_dict_Negative
 crack_Negative_df['label'] = 0
 crack_Negative_df['image'] = crack_Negative_df['path'].map(lambda x: np.asarray(Image.open(x).resize((75,100))))
 
-crack_Positive_df['path']=imageid_path_dict_Positive
-crack_Positive_df['label']=1
-crack_Positive_df['image']=crack_Positive_df['path'].map(lambda x: np.asarray(Image.open(x).resize((75,100))))
-
-print("Negative:",crack_Negative_df["image"][0].shape)
-print("Positive:",crack_Positive_df["image"][0].shape)
+crack_Positive_df['path'] = imageid_path_dict_Positive
+crack_Positive_df['label'] = 1
+crack_Positive_df['image'] = crack_Positive_df['path'].map(lambda x: np.asarray(Image.open(x).resize((75,100))))
+# Show information image [height, width, channels]
+print("Negative:", crack_Negative_df["image"][0].shape)
+print("Positive:", crack_Positive_df["image"][0].shape)
 
 print(imageid_path_dict_Negative.shape,imageid_path_dict_Positive.shape)
 
 crack_df = crack_Negative_df.append(crack_Positive_df)  # Combine two dataframes
-crack_df.reset_index(drop=True,inplace=True)  # Reset search
+crack_df.reset_index(drop=True, inplace=True)  # Reset search
 print(crack_df.shape)
-features=crack_df.drop(columns=['label'],axis=1)
-target=crack_df["label"]
+features = crack_df.drop(columns=['label'],axis=1)
+target = crack_df["label"]
 
 x_train_o, x_test_o, y_train, y_test = train_test_split(features, target, test_size=0.20,random_state=1234)
 
@@ -58,100 +58,96 @@ x_train = x_train.reshape(x_train.shape[0], *(75, 100, 3))
 x_test = x_test.reshape(x_test.shape[0], *(75, 100, 3))
 input_shape = (75, 100, 3)
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',padding = 'Same',input_shape=input_shape))
-model.add(Conv2D(32,kernel_size=(3, 3), activation='relu',padding = 'Same',))
-model.add(MaxPool2D(pool_size = (2, 2)))
-model.add(Dropout(0.25))
+# Convolution neural network model
+def myModel():
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',padding = 'Same',input_shape=input_shape))
+    model.add(Conv2D(32,kernel_size=(3, 3), activation='relu',padding = 'Same',))
+    model.add(MaxPool2D(pool_size = (2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(64, (3, 3), activation='relu',padding = 'Same'))
+    model.add(Conv2D(64, (3, 3), activation='relu',padding = 'Same'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(0.40))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+    model.summary()
+    # Compile model
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    # Train model
+    model.fit(x_train, y_train, validation_split=0.1, epochs=50, batch_size=256)
+    return model
 
-model.add(Conv2D(64, (3, 3), activation='relu',padding = 'Same'))
-model.add(Conv2D(64, (3, 3), activation='relu',padding = 'Same'))
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Dropout(0.40))
-
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
-model.summary()
-
-model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
-
-model.fit(x_train, y_train, validation_split=0.1,epochs=50, batch_size=256)
-
+# Show all information
+model = myModel()
 history = model.history
 print(history.history)
 
+# Show figure model accuracy
 fig, ax = plt.subplots()
-
 ax.plot(history.history['accuracy'], label='training accuracy')
 ax.plot(history.history['val_accuracy'], label='val accuracy')
-
 ax.set_title('model accuracy', {'size': 16})
 ax.set_xlabel('epoch', {'size': 16})
 ax.set_ylabel('accuracy', {'size': 16})
-
 plt.tick_params(labelsize=14)
 plt.legend(loc='lower right')
 plt.tight_layout()
 plt.show()
 
+# Show figure model loss
 fig, ax = plt.subplots()
-
 ax.plot(history.history['loss'], label='training loss')
 ax.plot(history.history['val_loss'], label='val loss')
-
 ax.set_title('model loss', {'size': 16})
 ax.set_xlabel('epoch', {'size': 16})
 ax.set_ylabel('loss', {'size': 16})
-
 plt.tick_params(labelsize=14)
 plt.legend(loc='upper right')
 plt.tight_layout()
 plt.show()
 
+# Show confusion matrix
 from sklearn.metrics import roc_curve
 import matplotlib.pyplot as plt
-
-from sklearn.preprocessing import label_binarize
 from sklearn.metrics import confusion_matrix, precision_score, accuracy_score,recall_score, f1_score,roc_auc_score
-
 y_score = model.predict_classes(x_test)
 y_score_1 = model.predict(x_test)
-
 import seaborn as sns
 cm = confusion_matrix(y_test, y_score)
 print('confusion_matrix\n', cm)
-
 print('accuracy:{}'.format(accuracy_score(y_test, y_score)))
 print('precision:{}'.format(precision_score(y_test, y_score)))
 print('recall:{}'.format(recall_score(y_test, y_score)))
 print('f1-score:{}'.format(f1_score(y_test, y_score)))
-
-f,ax=plt.subplots()
+f, ax = plt.subplots()
 sns.heatmap(cm,annot=True,ax=ax,fmt='.4g') # Draw a heat map
-
 ax.set_title('confusion matrix',fontsize=16) # title
-ax.set_xlabel('Predict',fontsize=16) # Xaxis
-ax.set_ylabel('True',fontsize=16) # Yaxis
+ax.set_xlabel('Predict',fontsize=16) # X axis
+ax.set_ylabel('True',fontsize=16) # Y axis
+
+# Show AUC valve
 fig, ax = plt.subplots(figsize=(5, 4.4))
 # AUC valve
 auc = roc_auc_score(y_test,y_score_1)
 # Draw curves
 fpr, tpr, thresholds = roc_curve(y_test,y_score_1)
 ax.plot(fpr, tpr, linewidth = 2,label='AUC=%.4f' % auc)
-ax.plot([0,1],[0,1], 'k--')
-
-
+ax.plot([0, 1], [0, 1], 'k--')
 ax.set_xlim(0, 1)
 ax.set_ylim(0, 1)
-ax.set_xlabel('False Postivie Rate',{'size':16})
-ax.set_ylabel('True Positive Rate',{'size':16})
-
-ax.plot([0,1],[0,1], 'k--',lw=2)
-
+ax.set_xlabel('False Postivie Rate', {'size':16})
+ax.set_ylabel('True Positive Rate', {'size':16})
+ax.plot([0, 1], [0, 1], 'k--', lw=2)
 plt.tick_params(labelsize=14)
-
 plt.legend()
 plt.tight_layout()
 plt.show()
+
+# Store the model as a pickle object
+pickle_out = open("model_trained.p", "wb")  # wb = WRITE BYTE
+pickle.dump(model, pickle_out)
+pickle_out.close()
+cv2.waitKey(0)
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import cv2
 import math
 import numpy as np
 import scipy.ndimage
-import Train
+import pickle
+####################
 
-
+font = cv2.FONT_HERSHEY_SIMPLEX
 def orientated_non_max_suppression(mag, ang):
     ang_quant = np.round(ang / (np.pi/4)) % 4
     winE = np.array([[0, 0, 0],[1, 1, 1], [0, 0, 0]])
@@ -28,47 +29,80 @@ def non_max_suppression(data, win):
     data_max[data != data_max] = 0
     return data_max
 
-# start calulcation
+# Create camera object
+cap = cv2.VideoCapture(0)
 
-gray_image = Camera.cap.read()
-with_nmsup = True #apply non-maximal suppression
-fudgefactor =1.3 #with this threshold you can play a little bit
-sigma = 21 #for Gaussian Kernel
-kernel = 2*math.ceil(2*sigma)+1 #Kernel size
+# Import the trained model
+pickle_in = open("model_trained.p", "rb")  ## rb = READ BYTE
+model = pickle.load(pickle_in)
 
-gray_image = gray_image/255.0
-blur = cv2.GaussianBlur(gray_image, (kernel, kernel), sigma)
-gray_image = cv2.subtract(gray_image, blur)
+while True:
+    _, frame = cap.read()
+    # start calulcation
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    with_nmsup = True #apply non-maximal suppression
+    fudgefactor = 1.3 #with this threshold you can play a little bit
+    sigma = 21 #for Gaussian Kernel
+    kernel = 2*math.ceil(2*sigma)+1 #Kernel size
 
-# compute sobel response
-sobelx = cv2.Sobel(gray_image, cv2.CV_64F, 1, 0, ksize=3)
-sobely = cv2.Sobel(gray_image, cv2.CV_64F, 0, 1, ksize=3)
-mag = np.hypot(sobelx, sobely)
-ang = np.arctan2(sobely, sobelx)
+    gray_image = gray_image/255.0
+    blur = cv2.GaussianBlur(gray_image, (kernel, kernel), sigma)
+    gray_image = cv2.subtract(gray_image, blur)
 
-# threshold
-threshold = 4 * fudgefactor * np.mean(mag)
-mag[mag < threshold] = 0
+    # compute sobel response
+    sobelx = cv2.Sobel(gray_image, cv2.CV_64F, 1, 0, ksize=3)
+    sobely = cv2.Sobel(gray_image, cv2.CV_64F, 0, 1, ksize=3)
+    mag = np.hypot(sobelx, sobely)
+    ang = np.arctan2(sobely, sobelx)
 
-#either get edges directly
-if with_nmsup is False:
-    mag = cv2.normalize(mag, 0, 255, cv2.NORM_MINMAX)
-    kernel = np.ones((5,5),np.uint8)
-    result = cv2.morphologyEx(mag, cv2.MORPH_CLOSE, kernel)
-    cv2.imshow('im', result)
-    cv2.waitKey()
+    # threshold
+    threshold = 4 * fudgefactor * np.mean(mag)
+    mag[mag < threshold] = 0
 
-#or apply a non-maximal suppression
-else:
+    # either get edges directly
+    if with_nmsup is False:
+        mag = cv2.normalize(mag, 0, 255, cv2.NORM_MINMAX)
+        kernel = np.ones((5, 5), np.uint8)
+        result = cv2.morphologyEx(mag, cv2.MORPH_CLOSE, kernel)
 
-    # non-maximal suppression
-    mag = orientated_non_max_suppression(mag, ang)
-    # create mask
-    mag[mag > 0] = 255
-    mag = mag.astype(np.uint8)
+        contours, _ = cv2.findContours(result, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cts = 0
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            if area > 300:
+                cv2.drawContours(result, cnt, -1, (0, 255, 0), 3)
+                cts += 1
+                print("Number of Contours: ", cts)
+            if cts < 2:
+                cv2.putText(result, "Ceramic tiles non-crack", (20, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+            else:
+                cv2.putText(result, "Ceramic tiles crack", (20, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.imshow('im', result)
+        cv2.waitKey()
 
-    kernel = np.ones((5,5),np.uint8)
-    result = cv2.morphologyEx(mag, cv2.MORPH_CLOSE, kernel)
+    # or apply a non-maximal suppression
+    else:
+        # non-maximal suppression
+        mag = orientated_non_max_suppression(mag, ang)
+        # create mask
+        mag[mag > 0] = 255
+        mag = mag.astype(np.uint8)
+        kernel = np.ones((5, 5), np.uint8)
+        result = cv2.morphologyEx(mag, cv2.MORPH_CLOSE, kernel)
 
-    cv2.imshow('im', result)
-    cv2.waitKey()
+        contours, _ = cv2.findContours(result, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cts = 0
+        for cnt in contours:
+            area = cv2.contourArea(cnt)
+            if area > 300:
+                cv2.drawContours(result, cnt, -1, (0, 255, 0), 3)
+                cts += 1
+                print("Number of Contours: ", cts)
+            if cts < 2:
+                cv2.putText(result, "Ceramic tiles non-crack", (20, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+            else:
+                cv2.putText(result, "Ceramic tiles crack", (20, 35), font, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+
+        cv2.imshow('im', result)
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
